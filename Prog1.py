dd46d088717e173fa4fc409b922526bb60e9ee08
Coro <200588478+Coro-sensei@users.noreply.github.com>
2025-03-08 00:03:59 +0800

    Update Prog1.py

--- Prog1.py
+++ Prog1.py
@@ -1,5 +1,5 @@
-num1 = float(input("First number:"))
+n1 = float(input("First number:"))
 
-num2 = float(input("Second number:"))
+n2 = float(input("Second number:"))
 
-print (max (num1, num2))
+print (max (n1, n2))
