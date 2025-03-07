80ba772ab9d6f6afd6da53812dad8691d67379a4
Coro <200588478+Coro-sensei@users.noreply.github.com>
2025-03-08 00:02:55 +0800

    Update Prog4.py

--- Prog4.py
+++ Prog4.py
@@ -1,5 +1,5 @@
-num1 = float(input("First number: "))
+n1 = float(input("First number: "))
 
-num2 = float(input("Second number: "))
+n2 = float(input("Second number: "))
 
-print (num1 * num2)
+print (n1 * n2)
